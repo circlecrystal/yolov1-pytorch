81234545eeb6565037d97713205f715aca34e2c6
Hahn Willen <circlecrystal@gmail.com>
2020-04-17 22:52:40 +0800

    updated.

--- predict.py
+++ predict.py
@@ -538,6 +538,7 @@ def predict_eval(test_file, model_name, weight,use_gpu=True):
         fps.append(open('%s_%s' % (base, voc_class_names[k]), 'w'))
 
     for imp in tqdm(lines):
+        print("imp: {}".format(imp))
         imgid = imp.split('/')[-1].split('.')[0]
         # print(imgid)
         img = cv2.imread(imp)
