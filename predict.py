d12353fe4f5ca732ac718056d0c0e626cdcf7a3c
Hahn Willen <circlecrystal@gmail.com>
2020-04-19 15:47:53 +0800

    Debugging...

--- predict.py
+++ predict.py
@@ -304,6 +304,7 @@ def get_pred_1(image,model_name,weight,mode=1,use_gpu=True):
         model.to(gpudevice)
     # h, w, _ = image.shape
     img = img_trans(image)
+    print("NN input: {}".format(input))
     if use_gpu:
         img = img.to(gpudevice)
     with torch.no_grad():
